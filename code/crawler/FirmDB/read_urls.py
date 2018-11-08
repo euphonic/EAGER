50bd05f39512d54b9db478467562df972ba214ed
Sanjay K. Arora <sarora@air.org>
2018-11-08 00:05:43 -0500

    cleaing urls

--- code/crawler/FirmDB/read_urls.py
+++ code/crawler/FirmDB/read_urls.py
@@ -1,10 +1,36 @@
 import csv
-import requests
-import pandas as pd
-from pws import Bing
 import FirmDB.settings
 import pprint
 from urllib.parse import urlparse
+import requests
+
+
+def fix_urls (firms):
+    s = requests.Session()
+    for firm in firms:
+        url = firm['url']
+        http_url = "http://" + url
+        resp = s.get(http_url)
+        if resp.status_code == 200:
+            firm['url'] = http_url
+            continue
+        http_www_url = "http://www." + url
+        resp = s.get(http_www_url)
+        if resp.status_code == 200:
+            firm['url'] = http_www_url
+            continue
+        https_url = "https://" + url
+        resp = s.get(https_url)
+        if resp.status_code == 200:
+            firm['url'] = https_url
+            continue
+        https_www_url = "https://www." + url
+        resp = s.get(https_www_url)
+        if resp.status_code == 200:
+            firm['url'] = https_www_url
+            continue
+
+    return firms
 
 def read_firms_csv (filename):
     """
@@ -33,10 +59,11 @@ def read_firms_csv (filename):
             firm['domain'] = urlparse(firm['url']).netloc
             firms.append(firm)
 
+    firms_fixed_urls = fix_urls (firms)
     pp = pprint.PrettyPrinter()
-    pp.pprint(firms)
+    pp.pprint(firms_fixed_urls)
     
-    return firms
+    return firms_fixed_urls
 
 if __name__ == "__main__":
 
