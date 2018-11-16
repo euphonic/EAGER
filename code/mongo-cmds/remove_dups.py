a9b09155a71b3d5f43c86b710af0be7396dbcf68
euphonic <sarora@air.org>
2018-11-16 15:54:07 +0000

    updating remove_dups script

--- code/mongo-cmds/remove_dups.py
+++ code/mongo-cmds/remove_dups.py
@@ -23,16 +23,31 @@ TARGET_COLLECTION = "pages_COMBINED"
 client = pymongo.MongoClient(connection_string, username=username, password=password, authSource=authSource,
                              authMechanism=authMechanism)
 db = client[MONGODB_DB]
+target_col = db[TARGET_COLLECTION]
 regex = re.compile('test|' + TARGET_COLLECTION)
+pp = pprint.PrettyPrinter()
 
 def dedup():
-    target_col = db[TARGET_COLLECTION]
     pipeline = [ { "$group": { "_id": { "name": "$url"}, "dups": { "$addToSet": "$_id" }, "count": { "$sum": 1 } }},
                  { "$match": { "count": { "$gt": 1 } }} ]
-    grouped_by_url = target_col.aggregate(pipeline)
-    print ('Found ' + str(len(grouped_by_url)) + ' documents with duplicate urls')
-    # pp = pprint.PrettyPrinter()
-    # pp.pprint(grouped_by_url)
+    grouped_by_url = list(target_col.aggregate(pipeline))
+    print ('Found ' + str(len(grouped_by_url)) + ' duplicate urls')
+    
+    to_delete_ids = []
+
+    for doc in grouped_by_url:
+        dups = doc['dups']
+        # print ('\tPopping: ' + str(dups.pop(0)))
+        to_delete_ids.append(dups)
+
+    flattened = [item for sublist in to_delete_ids for item in sublist] 
+    # pp.pprint(flattened)
+    return flattened
+
+def batch_delete (ids):
+    result = target_col.remove({'_id': {'$in': ids}})
+    pp.pprint (result)
 
 # run main
-dedup ()
+to_delete_ids = dedup ()
+batch_delete (to_delete_ids)
