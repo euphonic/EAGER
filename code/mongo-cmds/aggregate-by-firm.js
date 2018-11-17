77b7f28ce6ccf2e3aebfbb7e381dc1c4eb76eb1f
Sanjay K. Arora <sarora@air.org>
2018-11-16 21:01:27 -0500

    starting analysis

--- code/mongo-cmds/aggregate-by-firm.js
+++ code/mongo-cmds/aggregate-by-firm.js
@@ -2,3 +2,9 @@
 db.pages.aggregate(
 	([ { $group: {"_id":"$firm_name" , "number":{$sum:1}} } ])
 );
+
+# exclude nulls
+db.pages_COMBINED.aggregate(
+	([ { $match : { "firm_name" : { "$exists": true, "$ne": null }} },
+        { $group: {"_id":"$firm_name" , "number":{$sum:1}} } ])
+).toArray().length;
