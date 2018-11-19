9f935512d4af1612b32e664a80f8b18b5d3506f6
Sanjay K. Arora <sarora@air.org>
2018-11-19 11:35:40 -0500

    checking in assignee cleaned data

--- code/utilities/clean_orgs.py
+++ code/utilities/clean_orgs.py
@@ -1,10 +1,13 @@
 import csv
 import re
 
-f_in = open('/Users/sarora/dev/EAGER/data/linkedin/linkedin_orgs.txt')
+f_in = open('/Users/sarora/dev/EAGER/data/patents/assignee_extract.csv')
 csv_in = csv.reader(f_in)
 
+f_out = open('/Users/sarora/dev/EAGER/data/patents/assignee_clnd.csv', 'w')
+csv_out = csv.writer(f_out)
+
 for row in csv_in:
-    firm = row[0]
+    firm = row[4]
     firm_clnd = re.sub('(\.|,| corporation| incorporated| llc| inc| international| gmbh| ltd)', '', firm, flags=re.IGNORECASE).rstrip()
-    print (firm_clnd)
+    csv_out.writerow([row[0], row[1], row[2], row[3], firm, firm_clnd])
