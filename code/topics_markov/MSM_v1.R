3bfbfd057cdb327df8ce453f977de5a702bb3f34
Sanjay K. Arora <sarora@air.org>
2018-12-07 10:57:42 -0500

    adding MSM

---
+++ code/topics_markov/MSM_v1.R
@@ -0,0 +1,24 @@
+library("msm")
+setwd("/Users/sarora/dev/EAGER/data/orgs/depth0_pages/")
+
+in.depth0_topics <- read.csv("out_topics.csv", header = TRUE, stringsAsFactors = FALSE)
+head(in.depth0_topics)
+nrow(in.depth0_topics)
+in.depth0_topics$main_topic <- in.depth0_topics$main_topic + 1
+num_topics <- length(unique(in.depth0_topics$main_topic))
+num_topics
+unique(in.depth0_topics$main_topic)
+
+groups <- in.depth0_topics %>% group_indices(firm_key)
+in.depth0_topics$gid <- groups
+# View(in.depth0_topics)
+
+head(in.depth0_topics)
+statetable.msm(main_topic, gid, data = in.depth0_topics)
+
+qm <- matrix(rep (1, num_topics^2), nrow=num_topics, ncol=num_topics)
+diag(qm) <- 0
+qm
+
+eager.msm <- msm(main_topic ~ para_order, subject = gid, data = in.depth0_topics, qmatrix = qm, exacttimes = TRUE, gen.inits=TRUE)
+eager.msm
