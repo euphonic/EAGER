cbece4212f717d9f48f1f57996823049881f894e
euphonic <sarora@air.org>
2018-12-24 22:53:01 +0000

    replacing localhost from workshiop with the new aws mongodb on 2x.large

--- code/crawler/FirmDB/config.py
+++ code/crawler/FirmDB/config.py
@@ -1,4 +1,4 @@
-connection_string = 'mongodb://localhost'
+connection_string = 'mongodb://ec2-3-17-153-244.us-east-2.compute.amazonaws.com'
 username = "scrapy"
 password = "eager"
 authSource = "FirmDB"
