166c068e1f823ee5d39d8dc1579c2741f3a6b185
euphonic <sarora@air.org>
2019-01-29 07:56:39 +0000

    start crawling on workder

--- code/crawler/FirmDB/config.py
+++ code/crawler/FirmDB/config.py
@@ -1,4 +1,4 @@
-connection_string = 'mongodb://ec2-3-17-153-244.us-east-2.compute.amazonaws.com'
+connection_string = 'mongodb://ec2-18-223-152-229.us-east-2.compute.amazonaws.com'
 username = "scrapy"
 password = "eager"
 authSource = "FirmDB"
